21bb97347c3e3d8e7993ae58e7ac094e1c6e6907
Prateek Varma <prtk.varma@gmail.com>
2023-01-24 12:38:57 -0600

    Defined relationship between Job and Company schemas

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,5 +3,14 @@ import { Job } from "./db.js";
 export const resolvers = {
   Query: {
     jobs: () => Job.findAll()
+  },
+
+  Job: {
+    company: () => {
+      return {
+        id: 'fake',
+        name: 'Fake Inc.'
+      }
+    }
   }
 };
