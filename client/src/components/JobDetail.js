1e9d09f3c3344e7c09add62a87b9b6898e66a128
Prateek Varma <prtk.varma@gmail.com>
2023-01-30 15:29:47 -0600

    Query for individual job, and using React Hooks to display data

--- client/src/components/JobDetail.js
+++ client/src/components/JobDetail.js
@@ -1,11 +1,23 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import { jobs } from '../fake-data';
+import { getJob } from '../graphql/queries';
+//import { jobs } from '../fake-data';
 
 function JobDetail() {
+  const [job, setJob] = useState(null);
   const { jobId } = useParams();
 
-  const job = jobs.find((job) => job.id === jobId);
+  useEffect(() => {
+    getJob(jobId).then(setJob); //the funny setJob format is a shortcut to use the setter function
+  }, [jobId]);
+
+  console.log('[JobDetail] job: ', job);
+
+  if(!job) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <h1 className="title">
--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -2,6 +2,25 @@ import { request, gql } from 'graphql-request'; //client library
 
 const GRAPHQL_URL = 'http://localhost:9000/graphql';
 
+export async function getJob(id) {
+  const query = gql`
+    query JobQuery($id: ID!) {
+      job(id: $id) {
+          id
+          title
+          description
+          company {
+            id
+            name
+          }
+      }
+}
+  `;
+  const variables = { id };
+  const data = await request(GRAPHQL_URL, query, variables);
+  return data.job;
+}
+
 export async function getJobs() {
   const query = gql`
     query {
